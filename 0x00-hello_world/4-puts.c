af35011acf3d8e5c4c9dedf923319181768a0b2a
Richie-27 <rachealajayi27@gmail.com>
2022-03-10 16:30:41 -0800

    FPuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *
- *Description: main - Prints out string to stdout
- *
- *Return: 0 if successful
- *
+ * Description: main - Prints out string to stdout
+ * 
+ * Return: 0 if successful
+ * 
  */
 int main(void)
 {

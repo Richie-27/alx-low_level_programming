2855b516bdc0c301059e03aef71ef02609681d25
Richie-27 <rachealajayi27@gmail.com>
2022-03-10 16:08:41 -0800

    Using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 /*Using function puts to print*/
 # include <stdio.h>
-/** Description: main - Prints out a string to stdout.*/
+/**
+  * Description: main
+  * Prints out a string to stdout.
+  * /
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

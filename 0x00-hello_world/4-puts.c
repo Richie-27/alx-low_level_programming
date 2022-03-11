46f2c31963c5ea22197c2fec09eba2b13e7ddf50
Richie-27 <rachealajayi27@gmail.com>
2022-03-10 16:25:15 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *Description: main - Prints out string to stdout
@@ -7,13 +6,8 @@
  *Return: 0 if successful
  *
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }

2ea0fbc66994891e12b21911a47e5c29c7002f14
Richie-27 <rachealajayi27@gmail.com>
2022-03-11 05:31:26 -0800

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * Description: main- To print out a string on stdout.
+ * Return : 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
